42c1fdb2d3f0f5d49bd03e1a0719d7774c5f8a2a
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-11-19 17:38:01 -0800

    updated apiroutes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var userData = require("../data/friendsData");
+var friendsData = require("../data/friendsData");
 
 
 
@@ -27,8 +27,45 @@ module.exports = function(app) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-      userData.push(req.body);
-      res.json(userData);
+
+    var bestMatch = {
+      name: "",
+      photo: "",
+      friendDifference: 1000
+    };
+
+    // Here we take the result of the user"s survey POST and parse it.
+    var userData = req.body;
+    var userScores = userData.scores;
+
+    // This variable will calculate the difference between the user"s scores and the scores of
+    // each user in the database
+    var totalDifference = 0;
+
+    // Here we loop through all the friend possibilities in the database.
+    for (var i = 0; i < userData.length; i++) {
+
+      console.log(userData[i].name);
+      totalDifference = 0;
+
+      // We then loop through all the scores of each friend
+      for (var j = 0; j < userData[i].scores[j]; j++) {
+
+        // We calculate the difference between the scores and sum them into the totalDifference
+        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(userData[i].scores[j]));
+
+        // If the sum of differences is less then the differences of the current "best match"
+        if (totalDifference <= bestMatch.friendDifference) {
+
+          // Reset the bestMatch to be the new friend.
+          bestMatch.name = userData[i].name;
+          bestMatch.photo = userData[i].photo;
+          bestMatch.friendDifference = totalDifference;
+        }
+      }
+    }
+      friendsData.push(req.body);
+      res.json(bestMatch);
   });
 
 
