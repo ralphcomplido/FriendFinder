ea25739efabc3b53be37647d8f20e6e59309bd09
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-11-19 16:34:22 -0800

    fixed app.post

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -4,7 +4,7 @@
 // These data sources hold arrays of information on table-data, waitinglist, etc.
 // ===============================================================================
 
-var friendData = require("../data/friends");
+var userData = require("../data/friendsData");
 
 
 
@@ -20,9 +20,17 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(friendData);
+    res.json(userData);
   });
 
-  
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+      userData.push(req.body);
+      res.json(userData);
+  });
+
 
 };
+
