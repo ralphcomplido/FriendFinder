d9357da832a796fee66f647010421e0a0200c9dd
ralphcomplido <rcomplido.la.usa@gmail.com>
2017-11-20 23:57:33 -0800

    added friendsmatch algorithm

--- app/data/friendsData.js
+++ app/data/friendsData.js
@@ -15,7 +15,40 @@ var friendsArray = [
       4,
       1
     ]
+},
+{
+  "name":"Khan",
+  "photo":"http://leader.pubs.asha.org/data/Journals/ASHANL/934378/NIB1_web.png",
+  "scores":[
+      3,
+      2,
+      4,
+      4,
+      1,
+      1,
+      5,
+      5,
+      1,
+      1
+    ]
+},
+{
+  "name":"Ahmir",
+  "photo":"http://www.stanfordchildrens.org/content-public/topic/images/68/125868.gif",
+  "scores":[
+      2,
+      2,
+      3,
+      5,
+      1,
+      4,
+      2,
+      5,
+      4,
+      1
+    ]
 }
+
 ];
 
 // Note how we export the array. This makes it accessible to other files using require.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -20,54 +20,35 @@ module.exports = function(app) {
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
-    res.json(userData);
+    res.json(friendsData);
   });
 
   app.post("/api/friends", function(req, res) {
     // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
+    console.log(req.body);
+    console.log(friendsData);
 
-    var bestMatch = {
-      name: "",
-      photo: "",
-      friendDifference: 1000
-    };
+   	let curUserData = req.body
+   	var totalDiffArr = [];
+   	var sum = [];
+for (var i = 0; i < friendsData.length; i++) {
+	var friendsScores = friendsData[i].scores;
 
-    // Here we take the result of the user"s survey POST and parse it.
-    var userData = req.body;
-    var userScores = userData.scores;
+   	for (var j = 0; j < friendsScores.length; j++) {
+   		var subDiff = Math.abs(parseInt(friendsScores[j]) - parseInt(curUserData.scores[j]));
+   		totalDiffArr.push(subDiff);
+   		// console.log(sum);
+   		// console.log(totalDiffArr);
+   	}
 
-    // This variable will calculate the difference between the user"s scores and the scores of
-    // each user in the database
-    var totalDifference = 0;
-
-    // Here we loop through all the friend possibilities in the database.
-    for (var i = 0; i < userData.length; i++) {
-
-      console.log(userData[i].name);
-      totalDifference = 0;
-
-      // We then loop through all the scores of each friend
-      for (var j = 0; j < userData[i].scores[j]; j++) {
-
-        // We calculate the difference between the scores and sum them into the totalDifference
-        totalDifference += Math.abs(parseInt(userScores[j]) - parseInt(userData[i].scores[j]));
-
-        // If the sum of differences is less then the differences of the current "best match"
-        if (totalDifference <= bestMatch.friendDifference) {
-
-          // Reset the bestMatch to be the new friend.
-          bestMatch.name = userData[i].name;
-          bestMatch.photo = userData[i].photo;
-          bestMatch.friendDifference = totalDifference;
-        }
-      }
-    }
-      friendsData.push(req.body);
-      res.json(bestMatch);
-  });
-
+   	console.log(totalDiffArr);
+   	var totalDiff = totalDiffArr.reduce((a, b) => a + b, 0);
+   	friendsData[i].totalDiff = totalDiff;
+   	console.log(friendsData[i]);
+};
+     });
 
 };
 
